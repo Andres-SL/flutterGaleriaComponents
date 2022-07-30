ac34d547045dd564a11708602e5f7a6c8e6fea77
Andres-SL <co.andres.sl@gmail.com>
2022-07-29 21:50:18 -0500

    Primera version

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+import 'package:galeria_componentes/primeraclase.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Primera ejercicio con Flutter',
+    home: PrimerArchivo(),
+  ));
+}
+
+class PrimerArchivo extends StatelessWidget {
+  const PrimerArchivo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Primera App'),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AppBarDemo()),
+                    );
+                  },
+                  child: const Text('AppBar con Flutter'),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AppBarDemo()),
+                    );
+                  },
+                  child: const Text('Primer ejemplo con Flutter'),
+                ), 
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
